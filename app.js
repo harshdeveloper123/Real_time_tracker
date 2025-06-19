4d3a7d19bb3e8a6c87d92864ac528819f078f73d
harshdeveloper123 <harshsunami75@gmail.com>
2025-06-19 15:26:37 +0530

    updation

--- app.js
+++ app.js
@@ -2,26 +2,38 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const socketio = require("socket.io");
-const path = require("path"); // <-- Fix: Add this line
+const path = require("path");
+
 const server = http.createServer(app);
 const io = socketio(server);
 
+// Set EJS as the view engine
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public"))); // <-- also change `set` to `use`
-io.on("connection",function(socket){
-    socket.on("send location", function(data){
-        io.emit("receive-location",{id:socket.id, ...data})
-    })
-    // console.log("connected")
-    socket.on("disconnet",function(){
-        io.emit("user-dosconnected",socket.id)
-    })
+app.set("views", path.join(__dirname, "views")); // important!
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Socket.io logic
+io.on("connection", function (socket) {
+    console.log("User connected: " + socket.id);
+
+    socket.on("send location", function (data) {
+        io.emit("receive-location", { id: socket.id, ...data });
+    });
+
+    socket.on("disconnect", function () {
+        io.emit("user-disconnected", socket.id);
+    });
 });
-app.get("/", function(req, res) {
+
+// Render the frontend
+app.get("/", function (req, res) {
     res.render("index");
 });
 
-// server.listen(3000, () => {
-//     console.log("Server is running on http://localhost:3000");
-// });
-server.listen(3000)
+// Start server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
